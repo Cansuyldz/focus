937b7b73a78bbaceba3e7c8180f8873d72758265
Cansu Yıldız <109664594+Cansuyldz@users.noreply.github.com>
2022-10-09 12:55:33 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var c = 29;
+var f = 10;
+var toplam = (c + f) / 2;
+console.log(toplam);
+if (toplam >= 75 && toplam <= 100) {
+    console.log("5")
+} else if (toplam >= 50 && toplam <= 75) {
+    console.log("3")
+} else if (toplam < 50) {
+    console.log("1 kaldı")
+}
